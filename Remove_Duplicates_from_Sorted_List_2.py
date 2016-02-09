d94de412c482a791f6dde69e9735a416d0790aed
Fanghan Yang <yfhsjtu@gmail.com>
2016-02-09 00:13:19 -0500

    Create Remove_Duplicates_from_Sorted_List_2.py

---
+++ Remove_Duplicates_from_Sorted_List_2.py
@@ -0,0 +1,23 @@
+public class Solution {
+	public static ListNode deleteDuplicates(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		// dummy node
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		head = dummy;
+
+		while (head.next != null && head.next.next != null) {
+			if (head.val == head.next.next.val) {
+				int val = head.next.val;
+				while (head.next != null && head.next.val == val) {
+					head.next = head.next.next;
+				}
+			} else {
+				head = head.next;
+			}
+		}
+		return dummy.next;
+	}
+}
